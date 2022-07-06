c5d455ae54edd64bb06f341df794e40af49a141b
현준용 <hjy4649@cocen.com>
2022-07-07 08:52:22 +0900

    type script 첫 시작

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,5 +1,5 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = 'https://jsonplaceholder.typicode.com/users/';
 
 // dom
 var username = document.querySelector('#username');
@@ -13,9 +13,12 @@ function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
       user = response.data;
+      console.log(user);
       // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address;
     })
     .catch(function (error) {
       console.log(error);
