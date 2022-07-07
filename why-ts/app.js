ec3a56464726fc10185a0fb9cb16ff6d39c0596e
현준용 <hjy4649@cocen.com>
2022-07-08 08:21:27 +0900

    type 선언

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,9 +9,36 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ *
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ *
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ *
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then((res) => {
+  res.address.city;
+});
+
 function startApp() {
-  axios
-    .get(url)
+  fetchUser()
     .then(function (response) {
       user = response.data;
       console.log(user);
